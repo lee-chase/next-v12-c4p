39332b6b00824e328962ca91005968c0783a8df8
Lee Chase <lee.chase@uk.ibm.com>
2023-09-21 13:12:38 +0100

    chore: nextv12 data grid on ibm-products 2.7.0

---
+++ pages/config.js
@@ -0,0 +1,3 @@
+import { pkg } from '@carbon/ibm-products';
+// NOTE: must happen before component is first used
+pkg.component.Datagrid = true;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// import './_index.scss';
+
+import './config.js';
+
+import { Example } from '../components/Example';
+
+export default function Home() {
+  return (
+    <div className='app'>
+      <Example />
+    </div>
+  );
+}
